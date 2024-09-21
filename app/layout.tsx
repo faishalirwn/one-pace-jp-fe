83b6110958f71ec07f82f4bc2b1f8345afc1f137
Faishal Irawan <muhammadfaishal193@gmail.com>
2024-09-21 09:59:12 +0700

    fix: update website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "One Pace JP dev",
+    title: "One Pace JP",
     description: "One Pace japanese subtitle maker",
 };
 
